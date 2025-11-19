3d20f17ff0b5703575a18a9b6ca6a244e1cc842a
Sahil Adhikari <sahiladhikari39@gmail.com>
2025-11-19 13:32:21 +0545

    used map function as well as arrow function to update the site

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(noteDetail => <Note key={noteDetail.key} title={noteDetail.title} content={noteDetail.content} />)};
       <Footer />
     </div>
   );
